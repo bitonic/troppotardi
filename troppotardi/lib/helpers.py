f80e701a8604382a105238bd8946fe5ce668a09f
Francesco <e.imhotep@gmail.com>
2010-07-08 17:47:48 +0100

    Fixed google analytics

--- troppotardi/lib/helpers.py
+++ troppotardi/lib/helpers.py
@@ -16,4 +16,5 @@ flash = _Flash()
 from pylons import url
 
 def google_analytics():
-    return literal("<script type=\"text/javascript\">var _gaq = _gaq || []; _gaq.push(['_setAccount', '" + config['analytics_id'] + "']); _gaq.push(['_trackPageview']); (function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script>")
+    script = '\n<script type="text/javascript">\n\n  var _gaq = _gaq || [];\n  _gaq.push([\'_setAccount\', \'' + config['analytics_id'] + '\']);\n  _gaq.push([\'_trackPageview\']);\n\n  (function() {\n    var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;\n    ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';\n    var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);\n  })();\n\n</script>'
+    return literal(script)
