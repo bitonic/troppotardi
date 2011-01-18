56fe9d5c5978a42ed78e2f8783075ff0fc1ed45d
Francesco <e.imhotep@gmail.com>
2011-01-18 00:38:03 +0000

    Refixed and restructured thumbnailer.

--- troppotardi/lib/thumbnailer.py
+++ troppotardi/lib/thumbnailer.py
@@ -2,7 +2,56 @@ from PIL import Image
 import os
 from pylons import config
 
-def thumbnailer(filename, max_width=None, max_height=None, crop=False):
+def resize_image(filename, max_width=None, max_height=None, crop=False):
+    """Given the filename of an image in the image dir,
+    it resizes it"""
+
+    im = Image.open(os.path.join(config['images_dir'], filename))
+    (width, height) = im.size
+    
+    # If the image is smaller then the max size, we don't touch it
+    if not (width <= max_width and height <= max_height):
+        if crop and max_width and max_height:
+            if width / float(height) >= max_width / float(max_height):
+                thumb_height = max_height
+                thumb_width = width * max_height / height
+                sx = (thumb_width - max_width) / 2
+                up = 0
+                dx = sx + max_width
+                low = max_height
+            else:
+                thumb_width = max_width
+                thumb_height = max_width * height / width
+                sx = 0
+                up = (thumb_height - max_height) /2
+                dx = max_width
+                low = max_height + up
+
+            im.thumbnail((thumb_width, thumb_height), Image.ANTIALIAS)
+            im = im.crop((sx, up, dx, low))
+        else:
+            if max_height:
+                new_width = width * max_height / height
+            if max_width:
+                new_height = height * max_width / width
+                
+            if max_height and max_width:
+                if new_width > max_width:
+                    max_height = None
+                else:
+                    max_width = None
+                    
+            # Calculate the size...
+            if max_width and not max_height:
+                max_height = new_height
+            elif max_height and not max_width:
+                max_width = new_width
+                
+            im.thumbnail((max_width, max_height), Image.ANTIALIAS)
+    
+    return im
+
+def thumbnailer(filename, max_width=None, max_height=None, crop=False, save=True):
     """Given the filename of an image in the image dir,
     it thumbnails it and stores it in the thumbnails directory
     (both directories are defined in the ini file).
@@ -21,51 +70,8 @@ def thumbnailer(filename, max_width=None, max_height=None, crop=False):
     # This could be dangerous if the image could be changed, but
     # the image cannot be changed right now, so it should be safe.
     if not os.path.isfile(os.path.join(config['thumbs_dir'], name)):
-        
-        im = Image.open(os.path.join(config['images_dir'], filename))
-        (width, height) = im.size
+        im = resize_image(filename, max_width, max_height, crop)
 
-        # If the image is smaller then the max size, we don't touch it
-        if not (width <= max_width and height <= max_height):
-            if crop and max_width and max_height:
-                if width / float(height) >= max_width / float(max_height):
-                    thumb_height = max_height
-                    thumb_width = width * max_height / height
-                    sx = (thumb_width - max_width) / 2
-                    up = 0
-                    dx = sx + max_width
-                    low = max_height
-                else:
-                    thumb_width = max_width
-                    thumb_height = max_width * height / width
-                    sx = 0
-                    up = (thumb_height - max_height) /2
-                    dx = max_width
-                    low = max_height + up
-                    
-                im.thumbnail((thumb_width, thumb_height), Image.ANTIALIAS)
-                im = im.crop((sx, up, dx, low))
-            else:
-                if max_height:
-                    new_width = width * max_height / height
-                if max_width:
-                    new_height = max_height = height * max_width / width
-
-                if max_height and max_width:
-                    if new_width > max_width:
-                        max_height = None
-                    else:
-                        max_width = None
-                
-                # Calculate the size...
-                if max_width and not max_height:
-                    max_height = new_height
-                elif max_height and not max_width:
-                    max_width = new_width
-                
-                im.thumbnail((max_width, max_height), Image.ANTIALIAS)
-
-        # Save it 
         im.save(os.path.join(config['thumbs_dir'], name))
-            
+    
     return os.path.join(config['thumbs_base_url'], name)
